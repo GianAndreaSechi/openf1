830708db4e014d75cf89371dc7472fd549af3893
GianAndreaSechi <sechi90@hotmail.it>
2025-08-23 21:01:42 +0200

    feat(openf1-mcp): update healtz tools

--- mcp/src/tools/healthz_tools.py
+++ mcp/src/tools/healthz_tools.py
@@ -1,10 +1,12 @@
+import time
 from fastmcp import Context
 from loguru import logger
+from dto.McpResponse import McpResponse
 
 def register_tools(mcp):
     """Register all healthz tools for the MCP Server"""
 
     @mcp.tool("healthz")
-    async def healthz(ctx: Context) -> dict:
+    async def healthz(ctx: Context) -> McpResponse:
         """Use to know if MCP is alive"""
-        return {"ping": now()}
+        return McpResponse(message="Ping retrieved successfully", data={"ping": time.time()})
